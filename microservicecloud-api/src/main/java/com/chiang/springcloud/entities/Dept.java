794628077178f962f8eaca0b6012f7f7cd0765c3
jyp <jyp@192.168.0.5>
2019-04-23 22:12:20 +0800

    lombok

--- microservicecloud-api/src/main/java/com/chiang/springcloud/entities/Dept.java
+++ microservicecloud-api/src/main/java/com/chiang/springcloud/entities/Dept.java
@@ -2,75 +2,29 @@ package com.chiang.springcloud.entities;
 
 import java.io.Serializable;
 
-public class Dept implements Serializable{ //Entity orm
-	private Long 	deptno; // 主键
-	private String 	dname; // 部门名称
-	private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
-	public Dept(Long deptno, String dname, String db_source) {
-		super();
-		this.deptno = deptno;
-		this.dname = dname;
-		this.db_source = db_source;
-	}
-	public Dept() {
-		super();
-	}
-	public Long getDeptno() {
-		return deptno;
-	}
-	public void setDeptno(Long deptno) {
-		this.deptno = deptno;
-	}
-	public String getDname() {
-		return dname;
-	}
-	public void setDname(String dname) {
-		this.dname = dname;
-	}
-	public String getDb_source() {
-		return db_source;
-	}
-	public void setDb_source(String db_source) {
-		this.db_source = db_source;
-	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((db_source == null) ? 0 : db_source.hashCode());
-		result = prime * result + ((deptno == null) ? 0 : deptno.hashCode());
-		result = prime * result + ((dname == null) ? 0 : dname.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Dept other = (Dept) obj;
-		if (db_source == null) {
-			if (other.db_source != null)
-				return false;
-		} else if (!db_source.equals(other.db_source))
-			return false;
-		if (deptno == null) {
-			if (other.deptno != null)
-				return false;
-		} else if (!deptno.equals(other.deptno))
-			return false;
-		if (dname == null) {
-			if (other.dname != null)
-				return false;
-		} else if (!dname.equals(other.dname))
-			return false;
-		return true;
-	}
-	@Override
-	public String toString() {
-		return "Dept [deptno=" + deptno + ", dname=" + dname + ", db_source=" + db_source + "]";
-	}
+import org.apache.http.impl.execchain.MainClientExec;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@SuppressWarnings({ "unused", "serial" })
+// @AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Accessors(chain = true)
+public class Dept implements Serializable { // Entity orm
+	/**
+	 * 
+	 */
+	private Long deptno; // 主键
+	private String dname; // 部门名称
+	private String db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+
 	
+	public static void main(String[]args) {
+		Dept dpt = new Dept();
+		dpt.setDeptno(11L).setDname("RD").setDb_source("DB01");
+	}
 }
