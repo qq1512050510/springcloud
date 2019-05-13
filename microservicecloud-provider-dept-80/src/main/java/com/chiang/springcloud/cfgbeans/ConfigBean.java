63a19dc68d762a3f677d2c9a9d1a22c0de12f2c1
jiangyupeng <jiangyupeng@bjsasc.com>
2019-05-13 14:49:09 +0800

    consumer controller

---
+++ microservicecloud-provider-dept-80/src/main/java/com/chiang/springcloud/cfgbeans/ConfigBean.java
@@ -0,0 +1,14 @@
+package com.chiang.springcloud.cfgbeans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ConfigBean {//boot -->spring applicationContext.xml -->@Configuration配置
+//ConfigBean = applicationContext.xml	
+	@Bean
+	public RestTemplate getUserSerivce() {
+		return new RestTemplate();
+	}
+}
